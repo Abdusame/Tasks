113129c22f3014ea3561dca9947fa5934678c1e0
45OchilAZ <abdusame93@gmail.com>
2021-10-05 17:13:45 +0300

    Longest palindromic substring solution

--- src/leetcode/LongestPalindromicSubstring.java
+++ src/leetcode/LongestPalindromicSubstring.java
@@ -12,60 +12,46 @@ package leetcode;
 public class LongestPalindromicSubstring {
 
     public static void main(String[] args) {
-        System.out.println(longestPalindrome2("fwebabewdf"));
-        //System.out.println(isPalindrome("asbvbsa"));
+        LongestPalindromicSubstring l = new LongestPalindromicSubstring();
+        System.out.println(l.longestPalindrome("abb"));
     }
 
-    public static String longestPalindrome2(String s) {
-        String maxLengthSub = "";
-        String sub1;
-        String sub2;
-        String sub3;
-        for (int i = 0; i < s.length(); i++) {
-            sub1 = s.substring(i);
-            if (isPalindrome2(sub1)) {
-                maxLengthSub = sub1;
-                break;
-            }
-            sub2 = s.substring(0, s.length() - 1 - i);
-            if (isPalindrome2(sub2)) {
-                maxLengthSub = sub2;
-                break;
-            }
-            sub3 = s.substring(i, s.length() - 1 - i);
-            if (isPalindrome2(sub3)) {
-                maxLengthSub = sub3;
-                break;
-            }
-        }
-        return maxLengthSub;
-    }
-
-    public static String longestPalindrome(String s) {
+    /**
+     * Для длинных строк слишком медленно выполняется.
+     * */
+    public String longestPalindrome(String s) {
         String maxLengthSub = "";
         String sub;
-        for (int i = 0; i < s.length(); i++) {
-            sub = s.substring(i);
+        String sub1;
+        String sub2;
+        if (s.length() == 1) {
+            return s;
+        } else if (s.length() > 1 && isPalindrome(s)) {
+            return s;
+        }
+        for (int i = 0; i < s.length()/2; i++) {
+            sub = s.substring(i, s.length() - i);
+            //System.out.println("sub = " + sub);
             if (isPalindrome(sub) && sub.length() > maxLengthSub.length()) {
                 maxLengthSub = sub;
             }
+            for (int j = 0; j < sub.length(); j++) {
+                sub1 = sub.substring(j);
+                //System.out.println("sub1 = " + sub1);
+                if (isPalindrome(sub1) && sub1.length() > maxLengthSub.length()) {
+                    maxLengthSub = sub1;
+                }
+                sub2 = sub.substring(0, sub.length() - 1 - j);
+                //System.out.println("sub2 = " + sub2);
+                if (isPalindrome(sub2) && sub2.length() > maxLengthSub.length()) {
+                    maxLengthSub = sub2;
+                }
+            }
         }
         return maxLengthSub;
     }
 
-    public static boolean isPalindrome(String sub) {
-        char[] array = sub.toCharArray();
-        boolean isPal = true;
-        for (int i = 0; i < array.length / 2; i++) {
-            if (array[i] != array[array.length - 1 - i]) {
-                isPal = false;
-                break;
-            }
-        }
-        return isPal;
-    }
-
-    public static boolean isPalindrome2(String sub) {
+    public boolean isPalindrome(String sub) {
         boolean isPal = true;
         for (int i = 0; i < sub.length() / 2; i++) {
             if (sub.charAt(i) != sub.charAt(sub.length() - 1 - i)) {
