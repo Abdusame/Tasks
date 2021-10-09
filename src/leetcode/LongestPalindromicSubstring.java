52d9f7e4cfb2800f08757db1f49f6fc3e2bae180
45OchilAZ <abdusame93@gmail.com>
2021-10-09 22:00:47 +0300

    Longest palindromic substring correct solution

--- src/leetcode/LongestPalindromicSubstring.java
+++ src/leetcode/LongestPalindromicSubstring.java
@@ -17,9 +17,36 @@ public class LongestPalindromicSubstring {
     }
 
     /**
-     * Для длинных строк слишком медленно выполняется.
+     * correct solution
      * */
     public String longestPalindrome(String s) {
+        String lps = "";
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            String odd = expandPalindrome(s, i, i);
+            String even = expandPalindrome(s, i, i + 1);
+            if (lps.length() < odd.length()) {
+                lps = odd;
+            }
+            if (lps.length() < even.length()) {
+                lps = even;
+            }
+        }
+        return lps;
+    }
+
+    private String expandPalindrome(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return s.substring(l + 1, r);
+    }
+
+    /**
+     * Для длинных строк слишком медленно выполняется.
+     * */
+    public String longestPalindromeSlowSolution(String s) {
         String maxLengthSub = "";
         String sub;
         String sub1;
