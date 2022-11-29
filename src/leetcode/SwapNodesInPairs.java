810194b9053d291b9b9cdd493bbd8b862ba8ba77
45OchilAZ <abdusame93@gmail.com>
2022-11-29 19:45:21 +0300

    Swap nodes in pairs

---
+++ src/leetcode/SwapNodesInPairs.java
@@ -0,0 +1,58 @@
+package leetcode;
+
+public class SwapNodesInPairs {
+    public static void main(String[] args) {
+        /*ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4,
+                new ListNode(5, new ListNode(6, new ListNode(7, new ListNode(8))))))));*/
+        ListNode head = new ListNode(1, new ListNode(2));
+        for (int i = 1; i < 9; i++) {
+            System.out.print("\t" + i + "\t" + "|");
+        }
+        System.out.println();
+
+        ListNode head2 = swapPairs(head);
+
+        while (head2 != null) {
+            System.out.print("\t" + head2.val + "\t" + "|");
+            head2 = head2.next;
+        }
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        ListNode first = head;
+        if (head != null) {
+            ListNode second = head.next;
+            if (second != null) {
+                ListNode newHead = second;
+                ListNode dop = null;
+                while (true) {
+                    first.next = second.next;
+                    second.next = first;
+                    if (dop != null) {
+                        dop.next = second;
+                    }
+                    dop = first;
+                    if (first.next != null && first.next.next != null) {
+                        second = first.next.next;
+                        first = first.next;
+                    } else {
+                        break;
+                    }
+                }
+                return newHead;
+            } else {
+                return head;
+            }
+        } else {
+            return head;
+        }
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+}
---
+++ src/leetcode/TwoSum.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers nums and an integer target,
+ * return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution,
+ * and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ * */
+
+public class TwoSum {
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+//        int[] result = twoSum.twoSum(new int[]{3,2,4}, 6);
+//        System.out.println(result[0] + ", " + result[1]);
+
+        int[] result = twoSum.twoSumVersion1(new int[]{3,3}, 6);
+        System.out.println(result[0] + ", " + result[1]);
+    }
+
+    // version 2
+    public int[] twoSum(int[] nums, int target) {
+       return new int[]{};
+    }
+
+    public int[] twoSumVersion1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        map.forEach((k, v) -> System.out.println(k + ": " + v));
+
+        for (int i = 0; i < nums.length; i++) {
+            int f = target - nums[i];
+            if (map.containsKey(f) && map.get(f) != i) {
+                return new int[]{i, map.get(f)};
+            }
+        }
+        throw new IllegalArgumentException("asdf");
+    }
+}
